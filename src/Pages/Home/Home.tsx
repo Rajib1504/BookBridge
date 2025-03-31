48c392ae2bc1fe9d77f37e3560359486f3d8d5e2
mrakashsaha <mr.akashsaha@gmail.com>
2025-03-31 13:44:16 +0600

    Conflict issue fixing

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,12 +1,8 @@
-import Banner from "../../components/Banner";
-import BestSeller from "../../components/BestSeller";
-
 
 const Home = () => {
       return (
             <div>
-                  <Banner></Banner>
-                  <BestSeller></BestSeller>
+                  Home
             </div>
       );
 };
