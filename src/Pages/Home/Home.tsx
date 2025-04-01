866c97eb3b0a5e9abf040b2ba826c7ce263a4274
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-01 12:09:42 +0530

    bug fixed for shop by category

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -10,7 +10,7 @@ const Home = () => {
                 <Banner/>
                 <BestSeller/>
                 <Discount/>
-                <ShopByCategory/>
+                
             </div>
       );
 };
