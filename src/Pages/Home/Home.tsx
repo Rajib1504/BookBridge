b3493d98296713b6c8b1ecd84db821c620d18a3b
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-01 15:30:03 +0530

    Top Auther is ready for roll

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -13,7 +13,7 @@ const Home = () => {
                 <BestSeller/>
                 <Discount/>
                 <ShopByCategory/>
-                <TopAuther/>
+               
             </div>
       );
 };
