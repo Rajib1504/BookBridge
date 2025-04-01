7a233887d06b47453d8e1e60383a4237cf4b8b7c
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-01 15:19:14 +0530

    top auther component is created

--- src/Components/TopAuther.tsx
+++ src/Components/TopAuther.tsx
@@ -9,16 +9,16 @@ const TopAuther = () => {
       <aside className='col-start-3 col-span-2'>
         <img src={author} alt="" />
       </aside>
-      <aside className='col-start-6 col-span-3'>
-        <h6>Top Auther</h6>
-        <h4>The Weeknd</h4>
-        <p>David is an author of such sublime depth that his works have been known to cause a tear to fall from the eyes of even the hardest of hearts.
+      <aside className='col-start-6 col-span-3  py-16  text-left'>
+        <h6 className='uppercase font-Inter text-xs tracking-widest font-bold text-[#d62928] mb-3'>Top Auther</h6>
+        <h4 className='font-Gilda text-4xl mb-5'>The Weeknd</h4>
+        <p className='font-Gilda mb-3 text-sm'>David is an author of such sublime depth that his works have been known to cause a tear to fall from the eyes of even the hardest of hearts.
         </p>
-        <p>For years, He dreamed of writing thrillers the way he thought they should be written: complex, multi-genre stories with unique plots that ‘move’. Now he is one of the New York Times bestsellers author.
+        <p className='font-Gilda mb-3 text-xs text-gray-500 tracking-widest'>For years, He dreamed of writing thrillers the way he thought they should be written: complex, multi-genre stories with unique plots that ‘move’. Now he is one of the New York Times bestsellers author.
         </p>
         <img src={signature} alt="" />
       </aside>
-      <aside className='col-start-9 col-span-3 px-2 border-2 border-red-400 grid grid-cols-12'>
+      <aside className='col-start-9 col-span-3 px-2  grid grid-cols-12'>
             <img src={symbol} alt="" className='col-span-3 col-start-1' />
             <img src={book2} alt="" className='col-start-6 col-span-5 '/>
             <img src={book} alt="" className='col-span-5 col-start-1' />
