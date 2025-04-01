fdd84aa0171a372845bebdc24b4cae367ac51618
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-01 15:28:17 +0530

    top atuher is created

--- src/Components/TopAuther.tsx
+++ src/Components/TopAuther.tsx
@@ -6,10 +6,10 @@ import book2 from '../assets/Images/TopAuther/author-home-img3.webp'
 const TopAuther = () => {
   return (
     <div className="w-full bg-[#fbeded] py-8 grid grid-cols-12">
-      <aside className='col-start-3 col-span-2'>
+      <aside className='col-start-2 col-span-3 md:col-start-3 md:col-span-2 flex items-center md:block md:pr-2'>
         <img src={author} alt="" />
       </aside>
-      <aside className='col-start-6 col-span-3  py-16  text-left'>
+      <aside className='col-start-6 col-span-8 md:col-span-3  md:pl-5 py-16  text-left'>
         <h6 className='uppercase font-Inter text-xs tracking-widest font-bold text-[#d62928] mb-3'>Top Auther</h6>
         <h4 className='font-Gilda text-4xl mb-5'>The Weeknd</h4>
         <p className='font-Gilda mb-3 text-sm'>David is an author of such sublime depth that his works have been known to cause a tear to fall from the eyes of even the hardest of hearts.
@@ -18,7 +18,7 @@ const TopAuther = () => {
         </p>
         <img src={signature} alt="" />
       </aside>
-      <aside className='col-start-9 col-span-3 px-2  grid grid-cols-12'>
+      <aside className='col-start-9 col-span-3 px-2 hidden  md:grid grid-cols-12'>
             <img src={symbol} alt="" className='col-span-3 col-start-1' />
             <img src={book2} alt="" className='col-start-6 col-span-5 '/>
             <img src={book} alt="" className='col-span-5 col-start-1' />
