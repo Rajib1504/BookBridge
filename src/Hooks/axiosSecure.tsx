d4be538221335291d7d691759ab6b860abb75366
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2025-04-15 19:53:00 +0530

    data updated

--- src/Hooks/axiosSecure.tsx
+++ src/Hooks/axiosSecure.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosSecure = axios.create({
   // local api
-  baseURL: "http://localhost:9000",
+  // baseURL: "http://localhost:9000",
+  baseURL: "https://book-bridge-server-rho.vercel.app",
   // todo: need to add live api
 });
 
